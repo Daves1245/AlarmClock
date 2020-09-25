6b934848f5669dddf8612033257177e539b0ea32
David Santamaria <davidsantamaria1245@gmail.com>
2020-09-25 16:04:43 -0400

    rpi add-on

--- alarm.py
+++ alarm.py
@@ -1,5 +1,24 @@
+"""
+alarm.py: The callback function for when
+the alarm is set off. This should be editted
+to fit the individual's need.
+I have hooked up a buzzer to a Pi for the meantime
+but will look for other solutions.
+"""
+
 # The actual alarm - this should be called
 # to set off whatever alarm mechanism will be used
-def alarm_callback():
-    return
+from gpiozero import Buzzer, PWMOutputDevice
 
+freq = 100
+hour = 15
+minute = 32
+
+gpio_pin = 17 # We provide 3.3V to the buzzer
+
+def alarm_callback():
+    buzzer = PWMOutputDevice(gpio_pin, initial_value = 0.0, frequency = freq)
+    while True:
+        buzzer.on()
+        buzzer.off()
+    buzzer.close()

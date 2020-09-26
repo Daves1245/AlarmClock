25243775130e98557d21da9768ed112e54b5e314
David Santamaria <davidsantamaria1245@gmail.com>
2020-09-25 22:37:07 -0400

    Add personal alarm callback for Raspberry Pi

--- alarm.py
+++ alarm.py
@@ -8,17 +8,66 @@ but will look for other solutions.
 
 # The actual alarm - this should be called
 # to set off whatever alarm mechanism will be used
-from gpiozero import Buzzer, PWMOutputDevice
+from gpiozero import Buzzer, PWMOutputDevice, Button, LED
+from time import sleep
+import threading
+import signal
 
-freq = 100
+freq = 1000
 hour = 15
 minute = 32
 
-gpio_pin = 17 # We provide 3.3V to the buzzer
+led_gpio = 20
+buzzer_gpio = 26
+stop_gpio = 19
+snooze_gpio = 13
+
+# Is the alarm clock running
+on = True
+
+def sig_handler(signum, frame):
+    global on
+    on = False
+    print("")
+    return
+
+def stop_callback():
+    return
+
+def snooze_callback():
+    return
+
+def interrupt():
+    global on
+    stop = Button(stop_gpio)
+    snooze = Button(snooze_gpio)
+    while on:
+        if stop.is_pressed:
+            print("Stop pressed!")
+            stop.wait_for_release()
+            on = False
+        if snooze.is_pressed:
+            print("Snooze pressed!")
+            snooze.wait_for_release()
+    return
 
 def alarm_callback():
-    buzzer = PWMOutputDevice(gpio_pin, initial_value = 0.0, frequency = freq)
-    while True:
-        buzzer.on()
-        buzzer.off()
-    buzzer.close()
+    global on
+    stop = threading.Thread(target = interrupt)
+    stop.start()
+
+    signal.signal(signal.SIGINT, sig_handler)
+
+    with LED(led_gpio) as led, PWMOutputDevice(buzzer_gpio, initial_value = 0.0,
+            frequency = freq) as buzzer:
+        while on:
+            print("on")
+            buzzer.pulse()
+            led.on()
+            sleep(1)
+            print("off")
+            buzzer.off()
+            led.off()
+            sleep(1)
+        buzzer.close()
+alarm_callback()
